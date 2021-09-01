3e4d1e3eab51040b1988eda31f268ded3ed871b0
Velo <hartley.max@outlook.com>
2021-09-01 11:19:12 +0100

    add url redirecting logic

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, redirect, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_URI
 import os
+import random
+import string
 
 app = Flask(__name__)
 
@@ -15,8 +17,8 @@ db.create_all()
 
 class URL(db.Model):
     __tablename__ = 'urls'
-    shortened_url = db.Column(db.String(5000), primary_key=True)
-    url = db.Column(db.String(5000), nullable=False)
+    shortened_url = db.Column(db.Text, primary_key=True)
+    url = db.Column(db.Text, nullable=False)
 
     def __init__(self, url, shortened_url):
         self.url = url
@@ -28,15 +30,34 @@ def home():
     if request.method == "GET":
         return render_template('index.html')
     else:
-        url = URL(request.form["url"], request.form["url"])
+        url_posted = False
+        while not url_posted:
+            short_url = ''.join(random.choices(string.ascii_letters, k=15))
+            data = URL.query.filter_by(shortened_url=short_url)
+            if data.count() == 0:
+                url_posted = True
+        url = URL(request.form["url"], short_url)
         db.session.add(url)
         db.session.commit()
-        return render_template('index.html', url=url.shortened_url)
+        host = os.environ.get('BASE_HOST_URL', 'http://127.0.0.1:5000/')
+        return render_template('index.html', host=host, url=url.shortened_url)
 
 
-@ app.route('/urls')
+@app.route('/urls/')
 def index():
     data = URL.query.all()
     response = [{"url": element.url, "shortened_url": element.shortened_url}
                 for element in data]
     return jsonify({"data": response})
+
+
+@app.route('/<string:url_id>/')
+def redirect_to_url(url_id):
+    url = URL.query.filter_by(shortened_url=url_id).first()
+    if not url:
+        return redirect('/')
+    else:
+        if url.url[0:4] == 'http':
+            return redirect(url.url)
+        else:
+            return redirect(f'http://{url.url}')
